cb4f24ca03f923f1ffa42620a4082305a5e42832
inovasi solusi <inovasisolusi@inovasis-MacBook-Pro.local>
2017-08-23 09:45:34 +0700

    MacOS

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,12 +4,19 @@ import {
 	Text
 } from 'react-native';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 class Home extends React.Component {
 	render() {
 		return(
 			<View>
 				<Text>HomeTab</Text>
+
+				<Ionicons
+					name = 'md-alarm'
+					size = {30}
+					color = 'black' />
 			</View>
 		);
 	}
